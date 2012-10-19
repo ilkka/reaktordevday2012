155205a0cee1cd902647012d91814430e61b638a
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2012-10-19 11:58:13 +0300

    Implement uniq command

---
+++ Haskell/Uniq.hs
@@ -0,0 +1,9 @@
+uniq :: [String] -> [String]
+uniq [] = []
+uniq (x:xs)
+    | xs == []     = [x]
+	| head xs == x = uniq xs
+	| otherwise    = x : uniq xs
+
+work input = unlines (uniq (lines input))
+main = interact work
